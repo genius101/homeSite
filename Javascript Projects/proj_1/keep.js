7f07cdc8f94294df7d64d8147e7f171462c3d5dc
genius101 <habibejio@hotmail.com>
2022-10-09 18:29:33 +0100

    Adding first batch of the Proj

---
+++ Javascript Projects/proj_1/keep.js
@@ -0,0 +1,25 @@
+//Keeping this codes here to be re-used
+
+// function Person(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob);
+//     this.dateBio = function() {
+//         return `${this.dob.getFullYear}`
+//     }
+
+// }
+
+// Person.prototype.fullName = function() {
+//     return `${this.firstName} ${this.lastName}`
+// }
+
+// const person1 = new Person('James','Smith', '27 July 1967');
+// const person2 = new Person('Mary', 'Franklin', '5 November 1991')
+
+// console.log(person1.fullName())
+
+//DOM Multiple selector
+// const items = document.querySelectorAll('.item');
+
+// items.forEach((item) => console.log(item));
---
+++ Javascript Projects/proj_1/readMe.md
@@ -0,0 +1,2 @@
+This is a Basic Website, I shall be updating more details in here
+
